39b795136ef409fa7904b6e828095ca9f716d4e3
Debayan <kardebayan3@gmail.com>
2021-06-27 17:39:50 +0530

    CPH1859: Bringup for Pixel Experience

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,4 +16,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_CPH1859.mk
+    $(LOCAL_DIR)/aosp_CPH1859.mk
--- device.mk
+++ device.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
